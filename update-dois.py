fe4e0c97e570a3d279339ecb2f61df2426ad0775
Joyce Stack <joyce.stack@mendeley.com>
2016-02-01 17:13:36 +0000

    Merge remote-tracking branch 'origin/master'

--- update-dois.py
+++ update-dois.py
@@ -1,6 +1,7 @@
 from mendeley import Mendeley
 import yaml
 import ads
+import re
 
 with open('config.yml') as f:
     config = yaml.load(f)
@@ -19,6 +20,7 @@ mendeley_session = auth.authenticate(authorization_response)
 
 docs = mendeley_session.documents.iter()
 
+
 for d in docs:
 
    # find all documents that have an arXiv id but not a DOI
@@ -32,13 +34,36 @@ for d in docs:
         else:
             arxiv = d.identifiers['arxiv']
             if arxiv is not None:
+                if 'doi' in d.identifiers:
+                    doi = d.identifiers['doi']
 
-                if 'doi' not in d.identifiers:
-                    # look up the DOI using the arXiv id in ADS
-                    papers = list(ads.SearchQuery(arxiv = arxiv))
-                    for paper in papers:
-                        print paper.title
-                        print paper.identifier
+                     # print doi_val, arXiv_val
+                    if arxiv: arxiv = re.sub('v.$', '', arxiv)
+                    if doi:
+                        if (doi.startswith('10.') == False): doi = None
+                    if doi:
+                        papers = list(ads.SearchQuery(doi=doi,
+                                                      fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
+                    elif arxiv:
+                        papers = list(ads.SearchQuery(arXiv=arxiv,
+                                                      fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
+                    else:
+                        print "No DOI or ArXiv ID.";
+                    if len(papers) < 1:
+                        print"No valid paper found!";
+                    elif len(papers) > 1:
+                        print "Multiple matching papers!";
+                    else:
+                        print papers[0].title
 
-                    # update the mendeley document with the doi
-                    mendeley_session.documents.get(d.id)
+                # if 'doi' not in d.identifiers:
+                #     # look up the DOI using the arXiv id in ADS
+                #     papers = list(ads.SearchQuery(arxiv = arxiv))
+                #     for paper in papers:
+                #         print paper.title
+                #         print paper.identifier
+                #
+                #     # update the mendeley document with the doi
+                #     mendeley_session.documents.get(d.id)
+
+
