17a96ab3a8434a7f8e0266ad6d838a4c4f34ffbb
Joyce Stack <joyce.stack@mendeley.com>
2016-02-02 17:02:07 +0000

    Merge remote-tracking branch 'origin/master'

--- update-dois.py
+++ update-dois.py
@@ -3,6 +3,8 @@ import yaml
 import ads
 import re
 
+import pickle
+
 with open('config.yml') as f:
     config = yaml.load(f)
 
@@ -10,60 +12,79 @@ ads.config.token = config['adsToken']
 
 mendeley = Mendeley(config['clientId'], config['clientSecret'], 'http://localhost:5000/oauth')
 
-auth = mendeley.start_authorization_code_flow()
 
-print auth.get_login_url()
+def use_login():
 
-authorization_response = raw_input('Enter the full callback URL: ')
+    auth = mendeley.start_authorization_code_flow()
 
-mendeley_session = auth.authenticate(authorization_response)
+    print auth.get_login_url()
 
-docs = mendeley_session.documents.iter()
+    authorization_response = raw_input('Enter the full callback URL: ')
+
+    mendeley_session = auth.authenticate(authorization_response)
+
+    docs = [d for d in mendeley_session.documents.iter()]
+
+    pickle.dump(docs, open("dump", "wb"))
 
 
-for d in docs:
 
-   # find all documents that have an arXiv id but not a DOI
 
-    if d.identifiers is None:
-        print "No identifiers found!"
-    else:
+def find_docs_without_doi():
+    docs = pickle.load(open("dump"))
+    docs_without_doi = filter(lambda d: d.identifiers and 'doi' not in d.identifiers, docs)
 
-        if 'arxiv' not in d.identifiers:
-            print "No arXiv found!"
-        else:
-            arxiv = d.identifiers['arxiv']
-            if arxiv is not None:
-                if 'doi' in d.identifiers:
-                    doi = d.identifiers['doi']
+    # a list with all documents that have no doi, query by arxiv
+    for doc_without_doi in docs_without_doi:
+        if 'doi' in doc_without_doi.identifiers:
+            print "This should not happen", doc_without_doi.identifiers
+        if 'arxiv' in doc_without_doi.identifiers:
 
-                     # print doi_val, arXiv_val
-                    if arxiv: arxiv = re.sub('v.$', '', arxiv)
-                    if doi:
-                        if (doi.startswith('10.') == False): doi = None
-                    if doi:
-                        papers = list(ads.SearchQuery(doi=doi,
-                                                      fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
-                    elif arxiv:
-                        papers = list(ads.SearchQuery(arXiv=arxiv,
-                                                      fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
-                    else:
-                        print "No DOI or ArXiv ID.";
-                    if len(papers) < 1:
-                        print"No valid paper found!";
-                    elif len(papers) > 1:
-                        print "Multiple matching papers!";
-                    else:
-                        print papers[0].title
+            papers = list(ads.SearchQuery(arXiv=doc_without_doi.identifiers['arxiv'],fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
+            if len(papers) < 1:
+                print "No valid paper found!"
+            elif len(papers) > 1:
+                print "Multiple matching papers!"
+            else:
+                print papers[0].title
 
-                # if 'doi' not in d.identifiers:
-                #     # look up the DOI using the arXiv id in ADS
-                #     papers = list(ads.SearchQuery(arxiv = arxiv))
-                #     for paper in papers:
-                #         print paper.title
-                #         print paper.identifier
-                #
-                #     # update the mendeley document with the doi
-                #     mendeley_session.documents.get(d.id)
+
+def find_docs_without_arxiv():
+    docs = pickle.load(open("dump"))
+    docs_without_arxiv = filter(lambda d: d.identifiers and 'arxiv' not in d.identifiers, docs)
+
+    # a list with all documents that have no doi, query by arxiv
+    for doc_without_arxiv in docs_without_arxiv:
+        if 'arxiv' in doc_without_arxiv.identifiers:
+            print "This should not happen", doc_without_arxiv.identifiers
+        if 'doi' in doc_without_arxiv.identifiers:
+
+            papers = list(ads.SearchQuery(doi=doc_without_arxiv.identifiers['doi'],
+                                          fl='first_author, bibcode, identifier, abstract, year, volume, pub, issue, page, keyword'))
+            if len(papers) < 1:
+                print "No valid paper found!"
+            elif len(papers) > 1:
+                print "Multiple matching papers!"
+            else:
+                print papers[0].title
+
+
+
+if __name__ == "__main__":
+    find_docs_without_doi()
+    find_docs_without_arxiv()
+
+
+#         print "Document  before " , d.identifiers
+
+        # updated_document = document.update(identifiers=document.identifiers.update({'item3': 3}))
+        #
+        # print "Document  after " , updated_document.identifiers
+        #
+        # patched_document = mendeley_session.documents.get(updated_document.id)
+        #
+        # print "PATCHED   " , patched_document.identifiers
+
+
 
 
